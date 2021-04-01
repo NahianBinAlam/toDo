23ed16f6d0df8efc6d124a64d79b5ed5d3a93b70
NahianBinAlam <36653955+NahianBinAlam@users.noreply.github.com>
2021-04-01 20:30:59 +0600

    setting up the urls and views

--- todoList/views.py
+++ todoList/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 def index(request):
-	return HttpResponse('hello world')
+
+	context = {}
+	return render(request, 'todoList/list.html', context)
+
+def update(request):
+
+	context = {}
+	return render(request, 'todoList/update.html', context)
+
+def delete(request):
+
+	context = {}
+	return render(request, 'todoList/delete.html', context)
